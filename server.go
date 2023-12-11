b997fe585817eb875f9220aaecd526440a04d67c
Tom Solem <tom.solem@eviny.no>
2023-12-11 07:49:16 +0100

    First setup

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, world!")
+	})
+
+	http.ListenAndServe(":5000", nil)
+}
